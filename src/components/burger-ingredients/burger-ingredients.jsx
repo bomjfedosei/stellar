7407bd71bcecf357c9fed51236e0df9a73b73e3a
bomjfedosei <48622689+bomjfedosei@users.noreply.github.com>
2023-12-23 16:31:44 +0300

    Траблы с переменной

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -17,7 +17,6 @@ const BurgerIngredients = ({ title, ingridients }) => {
 
   const onTabClick = (tabType, entry) => {
     setCurrent(tabType);
-    //entry.target.scrollIntoView({ behavior: "smooth" });
   };
 
   useEffect(() => {
@@ -43,21 +42,21 @@ const BurgerIngredients = ({ title, ingridients }) => {
       <div className={`${styles.tabs} pt-5`}>
         <Link to="bun" spy={true} smooth={true} offset={0} duration={800} containerId="containerElement"
           onSetActive={() => setCurrent('Булки')}>
-          <Tab value="Булки" active={current === 'Булки'} onClick={()=> onTabClick("Булки", bunsRef)}>Булки</Tab>
+          <Tab value="Булки" active={current === 'Булки'} onClick={()=> onTabClick("Булки")}>Булки</Tab>
         </Link>
         <Link to="main" spy={true} smooth={true} offset={30} duration={800} containerId="containerElement"
           onSetActive={() => setCurrent('Начинки')}>
-          <Tab value="Начинки" active={current === 'Начинки'} onClick={()=> onTabClick("Начинки", mainsRef)}>Начинки</Tab>
+          <Tab value="Начинки" active={current === 'Начинки'} onClick={()=> onTabClick("Начинки")}>Начинки</Tab>
         </Link>
         <Link to="sauces" spy={true} smooth={true} offset={28} duration={800} containerId="containerElement"
           onSetActive={() => setCurrent('Соусы')}>
-          <Tab value="Соусы" active={current === 'Соусы'} onClick={()=> onTabClick("Соусы", saucesRef)}>Соусы</Tab>
+          <Tab value="Соусы" active={current === 'Соусы'} onClick={()=> onTabClick("Соусы")}>Соусы</Tab>
         </Link>
       </div>
       <div className={`${styles.cardsContainer} custom-scroll`} id="containerElement">
-        <BurgerIngredientsGroup id="bun" title='Булки' ingridients={bun} bunsRef={bunsRef}/>
-        <BurgerIngredientsGroup id="main" title='Начинки' ingridients={main} bunsRef={mainsRef}/>
-        <BurgerIngredientsGroup id="sauces" title='Соусы' ingridients={sauces} bunsRef={saucesRef}/>
+        <BurgerIngredientsGroup id="bun" title='Булки' ingridients={bun} Ref={bunsRef}/>
+        <BurgerIngredientsGroup id="main" title='Начинки' ingridients={main} Ref={mainsRef}/>
+        <BurgerIngredientsGroup id="sauces" title='Соусы' ingridients={sauces} Ref={saucesRef}/>
       </div>
     </div>
   );
