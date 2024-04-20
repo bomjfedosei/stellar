c58efc33fd20468d5823e1ea7b8c7a9fd4bf4f82
bomjfedosei <48622689+bomjfedosei@users.noreply.github.com>
2024-04-21 01:20:09 +0300

    sprint-3

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,40 +1,37 @@
-import AppHeaderItem from '../app-header-item/app-header-item';
-import { BurgerIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import AppHeaderItem from '../app-header-item/app-header-item'
+import { BurgerIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 import { ListIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import { ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './app-header.module.css'
-import React from 'react';
-
-
 
 function AppHeader() {
-  const [active, setActive] = React.useState('Конструктор');
-
 
   return (
-    <header className={styles.header} >
+    <div className={styles.header} >
       <div className={styles.wrapper}>
+
         <AppHeaderItem
-          onClick={() => setActive("Конструктор")}
-          isActive={active === "Конструктор"}
+          path='/'
           text="Конструктор"
-          icon={<BurgerIcon />} />
+          icon={<BurgerIcon />}
+        />
+
         <AppHeaderItem
-          onClick={() => setActive("Лента Заказов")}
-          isActive={active === "Лента Заказов"}
+          path='/orders'
           text="Лента Заказов"
           icon={<ListIcon />} />
+
         <div className={styles.logo}>
           <Logo />
         </div>
+
         <AppHeaderItem
-          onClick={() => setActive("Личный кабинет")}
-          isActive={active === "Личный кабинет"}
+          path='/profile'
           text="Личный кабинет"
           icon={<ProfileIcon />} />
       </div>
-    </header >
+    </div >
   );
 }
 
--- src/components/burger-ingredients-group/burger-ingredients-group.jsx
+++ src/components/burger-ingredients-group/burger-ingredients-group.jsx
@@ -1,31 +1,20 @@
 import styles from './burger-ingredients-group.module.css';
 import cardTypes from '../../utils/propsType';
 import PropTypes from 'prop-types';
-
 import BurgerIngredientCard from '../burger-ingredient-card/burger-ingredient-card';
 
-function BurgerIngredientsGroup({ title, ingridients, id, Ref }) {
+const BurgerIngredientsGroup = ({ title, ingridients, name }) => {
 
     return (
-        <div className='pt-10' id={id} ref={Ref}>
-            <h1 className={styles.title}>{title}</h1>
+        <div className='mt-10' name={name}>
+            <h1 className={styles.title} >{title}</h1>
+
             <div className={styles.cards}>
                 {ingridients
                     .map((ingridient) => (
                         <BurgerIngredientCard
                             key={ingridient._id}
-                            // _id={ingridient._id}
-                            // type={ingridient.type}
-                            // name={ingridient.name}
-                            // image={ingridient.image}
-                            // image_large={ingridient.image_large}
-                            // image_mobile={ingridient.image_mobile}
-                            // calories={ingridient.calories}
-                            // carbohydrates={ingridient.carbohydrates}
-                            // fat={ingridient.fat}
-                            // proteins={ingridient.proteins}
-                            // price={ingridient.price}
-                            ingridient = {ingridient}
+                            {...ingridient}
                         />
                     ))}
             </div>
@@ -35,6 +24,7 @@ function BurgerIngredientsGroup({ title, ingridients, id, Ref }) {
 
 BurgerIngredientsGroup.propTypes = {
     title: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
     ingridients: PropTypes.arrayOf(cardTypes.isRequired).isRequired
 };
 
