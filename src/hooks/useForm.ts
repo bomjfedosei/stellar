19be17aeb229450bc78e92f3c6f05b28007aa484
Boris Lopatin <blopatin@lean-it.ru>
2024-05-19 21:36:17 +0300

    TSX ident

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,9 +1,13 @@
 import { useState } from "react";
 
-export const useForm = (inputValues) => {
+interface IAnswer {
+    [name: string]: string;
+}
+
+export function useForm<T extends IAnswer>(inputValues: T) {
     const [formValues, setFormValues] = useState(inputValues);
 
-    const handleInputsChange = event => {
+    const handleInputsChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
         const { value, name } = event.target;
         setFormValues({ ...formValues, [name]: value });
     };
